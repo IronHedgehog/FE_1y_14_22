21438f37521766bd250e3bf460e31d31fb1b634c
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2023-12-17 14:18:12 +0200

    dz

---
+++ js/dz.js
@@ -0,0 +1,6 @@
+// Створити змінну з іменем isStudent і присвоїти їй значення true або false, залежно від того, чи є студентом. Вивести значення цієї змінної в консоль.
+const isStudent = confirm("Чи є ви студентом?");
+
+// confirm,prompt,alert
+
+console.log(true);
