183809b65496d304306e32a369458df2e0ed4c59
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2023-12-17 16:00:14 +0200

    Numbers

--- js/index.js
+++ js/index.js
@@ -53,17 +53,45 @@ const num6 = mid !== "50"; // false
 const num7 = mid != "50"; // true
 const isFalse = !true; // false
 
-console.log(num6);
-console.log(num7);
-console.log(isFalse);
+// console.log(num6);
+// console.log(num7);
+// console.log(isFalse);
 
 // NAN - not a number = не число
 
 // ------------------------Числа(Number)--------------------
-
 // Приведення до числа( явне і не явне)
+
+// Тому що ми використовуєм неявні приведення до числа наш код важко читати
+
+const isString = +"10"; // Не явне приведення до числа(Складно прочитати )
+const isNumber1 = Number("100"); // 100
+const isNumber = Number("100і"); // NAN// явне приведення до числа,Може приводити до числа тільки строки які містять тільки число
+console.log(isNumber);
+
 // Взяти число з строки (parseint, parsefloat)
+
+const stringNum = "1.2йівфівфіasd3"; // num
+console.log(stringNum);
+//Number.PARSEINT(cтроку яку бажаєте привести до числа) Повертає цілі чісла
+// parse = перебирає отриманні данні і вибирає числа
+//int = integer = ціле число
+
+console.log(Number.parseInt(stringNum)); // 1
+
+// Parse float = повертає дробові числа(числа з плаваючою крапкою)
+console.log(Number.parseFloat(stringNum)); // 1.2
+
 // Перевірка на число (isNaN)
+//NaN = не число
+// метод isNan повертає нам значення булеве (true або false)
+// Якщо не число === true
+console.log(Number.isNaN("asdasd")); // true
+// якщо число === false
+console.log(Number.isNaN(100)); // false
+
+Number.isNaN("asdasd"); // true
+Number.isNaN(100); // false
 
 // ------------ДОДАВАННЯ ЧИСЕЛ З ПЛАВАЮЧОЮ ТОЧКОЮ-----------
 // 0.1, 0.2 ,0.3, 0.4.
