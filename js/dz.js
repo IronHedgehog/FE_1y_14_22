07d969775dd1dfe79cedfce479389e64166d6295
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2023-12-24 14:23:20 +0200

    dz

---
+++ js/dz.js
@@ -0,0 +1,20 @@
+const float = 2.9;
+
+//.floor = Завжди округлюэ до меншого
+console.log(Math.floor(float));
+
+const floatString = "12.2asdasd";
+const integerString = "10asdas";
+
+console.log(Number.parseFloat(floatString));
+console.log(Number.parseInt(integerString));
+
+const numbersqrt = 10;
+
+console.log(Math.sqrt(numbersqrt));
+
+const integer = 10;
+const subString = "10";
+
+console.log(Number.parseInt(subString));
+console.log(integer.toString());
