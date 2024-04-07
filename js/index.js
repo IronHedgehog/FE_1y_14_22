190844cb5bed8524e78fe81db848b538eadd6ce2
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-04-07 16:09:37 +0300

    css

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 // Impera
+// impera - опис кожного кроку
 
 const number = [1, 2, 3, 4, 5];
 
@@ -120,3 +121,127 @@ console.log(con.every((item) => item <= 10));
 
 // Перевіряє чи пройшов перевірку ХОЧА Б ОДИН елемент масиву тоді some повертає true ЯКЩО ВСІ НЕ ПРОЙШЛИ ПОВЕРТАЄ FALSE
 console.log(con.some((item) => item <= 10));
+
+// reduce,sort
+
+const users = [
+  {
+    id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    skills: ["ipsum", "lorem"],
+    gender: "male",
+    age: 37,
+  },
+  {
+    id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+    gender: "female",
+    age: 34,
+  },
+  {
+    id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+    gender: "male",
+    age: 24,
+  },
+  {
+    id: "249b6175-5c30-44c6-b154-f120923736f5",
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    skills: ["adipisicing", "irure", "velit"],
+    gender: "female",
+    age: 21,
+  },
+  {
+    id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    skills: ["ex", "culpa", "nostrud"],
+    gender: "male",
+    age: 27,
+  },
+  {
+    id: "150b00fb-dd82-427d-9faf-2879ea87c695",
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    skills: ["non", "amet", "ipsum"],
+    gender: "male",
+    age: 38,
+  },
+  {
+    id: "e1bf46ab-7168-491e-925e-f01e21394812",
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    skills: ["lorem", "veniam", "culpa"],
+    gender: "female",
+    age: 39,
+  },
+];
+
+// акумулятор це початкове значення з якого все починається
+
+//reduce приймає першим параметром акумулятор другим поточние елемент перебору, index, array
+//
+const totalBalance = users.reduce((acc, user) => {
+  return acc + user.balance;
+}, 0);
+
+console.log(totalBalance);
+// Викликає помилку
+// const newArr = users.reduce((acc, user) => {
+//   console.log(acc.push(users));
+// }, []);
+
+// const newObj = users.reduce((acc, item) => {
+//   console.log(acc);
+//   acc.gender = item.gender;
+//   console.log(acc);
+// }, {});
+// не викликає помилку
+// Перебираючі методи це також функції, якщо вони нічого не повертають з них вілитає в основному undefined
+const newArr = users.reduce((acc, user) => {
+  acc.push(user);
+  return acc;
+}, []);
+
+console.log(newArr);
+
+const newObj = users.reduce((acc, item) => {
+  acc.gender = item.gender;
+  return acc;
+}, {});
+
+console.log(newObj);
