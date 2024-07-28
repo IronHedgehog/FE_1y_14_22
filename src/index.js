6ff701621d0bce3f0d7c8ca3748f897bfd1a59f4
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-07-28 17:04:19 +0300

    lib

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import Chart from 'chart.js/auto';
+
 // import { nanoid } from 'nanoid';
-// const model = {};
-// console.log((model.id = nanoid(3)));
+// console.log(nanoid());
+
 import Toastify from 'toastify-js';
 import 'toastify-js/src/toastify.css';
 
@@ -18,3 +20,42 @@ Toastify({
   },
   onClick: function () {}, // Callback after click
 }).showToast();
+
+import {
+  alert,
+  defaultModules,
+} from '../node_modules/@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+
+defaultModules.set(PNotifyMobile, {});
+
+alert({
+  text: 'Notice me, Yura!',
+});
+
+//
+
+const canva = document.querySelector('#chart');
+
+const data = [
+  { year: 2010, count: 10 },
+  { year: 2011, count: 20 },
+  { year: 2012, count: 15 },
+  { year: 2013, count: 25 },
+  { year: 2014, count: 22 },
+  { year: 2015, count: 30 },
+  { year: 2016, count: 28 },
+];
+
+const chart = new Chart(canva, {
+  type: 'line',
+  data: {
+    labels: data.map(row => row.year),
+    datasets: [
+      {
+        label: 'Acquisitions by year',
+        data: data.map(row => row.count),
+      },
+    ],
+  },
+});
